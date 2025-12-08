21b9aaea016639320e46f19bcdd42319a4f4fba2
Julian Coleman <jdc@coris.org.uk>
2025-12-08 07:30:47 +0100

    Handle missing filename for uploads.

--- cgi-bin/upload.sh
+++ cgi-bin/upload.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
-# !/usr/bin/with-contenv bashio
 #
-# Upload the configuration files
+# Upload a configuration file
 
-# Our parameters
 CONFIG_DIR=/config
 OVPN=$CONFIG_DIR/client.ovpn
 TEXT=$CONFIG_DIR/client.text
@@ -15,17 +13,20 @@ elif [ "$QUERY_STRING" = "text=" ]; then
     FILE=$TEXT
 fi
 
-# Header for the response
 echo Content-type: text/plain
 echo
+
+echo -n > $TMP
+if [ $? -ne 0 ]; then
+    echo Upload failed: create error
+    exit
+fi
 
 # Find the separator from $CONTENT_TYPE
 sep="${CONTENT_TYPE#*boundary=}"
 
 # The file is the lines between the content type + 1 and separator - 1
 # Write this to a temporary file
-rm -f $TMP
-(
 found=0
 count=0
 output=0
@@ -33,11 +34,22 @@ while read line;
 do
     count=$((count + 1))
 
+    # Check the file name
+    case "$line" in
+        Content-Disposition*filename=\"\"*)
+            echo Upload failed: missing file name
+            exit
+        ;;
+    esac
+
     # We want text/plain or application/octet-stream
     case "$line" in
         Content-Type:*text/plain*|Content-Type:*application/octet-stream*)
             found=$((count + 1)) # Skip following empty line
         ;;
+        Content-Type:*)
+            echo Upload failed: wrong file type
+            exit
     esac
 
     # The last 2 lines are blank then separator, so skip them
@@ -49,7 +61,7 @@ do
             *)
                 # Don't output the previous line the first time
                 if [ $output -eq 1 ]; then
-                    echo $prev
+                    echo $prev >> $TMP
                 fi
                 prev="$line"
                 output=1
@@ -57,9 +69,8 @@ do
         esac
     fi
 done
-) > $TMP
 if [ $? -ne 0 ]; then
-    echo Upload failed: create failed
+    echo Upload failed: write error
     exit
 fi
 
@@ -67,11 +78,10 @@ fi
 chmod 0644 $TMP
 mv $TMP $FILE
 if [ $? -ne 0 ]; then
-    echo Upload failed: rename failed
+    echo Upload failed: rename error
     exit
 fi
 
 echo Upload succeeded
 cd $CONFIG 
 ls -l $FILE
-cat $FILE
