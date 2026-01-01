46ca17b54c72c0f589cab912d2784599a47fdf83
Julian Coleman <jdc@coris.org.uk>
2026-01-01 13:44:02 +0100

    Change red/green backgrounds to match HA colours.

--- openvpn_client/index.js
+++ openvpn_client/index.js
@@ -1,4 +1,7 @@
 var statuses = ""
+var colour_ok = "#43a047"
+var colour_nok = "#db4437"
+var colour_white = "#ffffff"
 
 function reqListener() {
   statuses = this.responseText;
@@ -6,30 +9,36 @@ function reqListener() {
   // .ovpn file
   if (status[0].endsWith("YES")) {
     document.getElementById("ovpn-status").innerHTML = "Uploaded";
-    document.getElementById("ovpn-status").style.backgroundColor = "#88ff88";
+    document.getElementById("ovpn-status").style.backgroundColor = colour_ok;
+    document.getElementById("ovpn-status").style.color = colour_white;
     document.getElementById("ovpn-download").style.display = "inline-block";
   } else {
     document.getElementById("ovpn-status").innerHTML = "Missing";
-    document.getElementById("ovpn-status").style.backgroundColor = "#ff8888";
+    document.getElementById("ovpn-status").style.backgroundColor = colour_nok;
+    document.getElementById("ovpn-status").style.color = colour_white;
     document.getElementById("ovpn-download").style.display = "none";
   }
   // .text file
   if (status[1].endsWith("YES")) {
     document.getElementById("text-status").innerHTML = "Uploaded";
-    document.getElementById("text-status").style.backgroundColor = "#88ff88";
+    document.getElementById("text-status").style.backgroundColor = colour_ok;
+    document.getElementById("text-status").style.color = colour_white;
     document.getElementById("text-download").style.display = "inline-block";
   } else {
     document.getElementById("text-status").innerHTML = "Missing";
-    document.getElementById("text-status").style.backgroundColor = "#ff8888";
+    document.getElementById("text-status").style.backgroundColor = colour_nok;
+    document.getElementById("text-status").style.color = colour_white;
     document.getElementById("text-download").style.display = "none";
   }
   // .pid file
   if (status[2].startsWith("pid=")) {
     document.getElementById("process-status").innerHTML = "Running";
-    document.getElementById("process-status").style.backgroundColor = "#88ff88";
+    document.getElementById("process-status").style.backgroundColor = colour_ok;
+    document.getElementById("process-status").style.color = colour_white;
   } else {
     document.getElementById("process-status").innerHTML = "Not running";
-    document.getElementById("process-status").style.backgroundColor = "#ff8888";
+    document.getElementById("process-status").style.backgroundColor = colour_nok;
+    document.getElementById("process-status").style.color = colour_white;
   }
 }
 
